22546e6adf48b5a72fd8693e634367cf9fde2898
Illia Chernous <illyachernous@gmail.com>
2020-07-13 21:56:45 +0300

    added HW1

---
+++ HW1/src/Task3.java
@@ -0,0 +1,19 @@
+public class Task3 {
+
+	public static void main (String[] args) {
+		
+int[] array = {1, 2, 3, 4, 5, 6,};
+
+	System.out.println("first variant:" + "\n");
+
+for (int i = 0; i < array.length; i++) {
+    System.out.println(array[i]);
+		}	
+	
+	System.out.println("\n" + "second variant:" + "\n");
+	
+for (int i = 0; i < array.length; i++) {
+	System.out.print(array[i] + " ");
+		}
+	}
+} 
